cd32b3703fa857111b1c13658ea9a7de30315ed1
FTS427 <FTS427@outlook.com>
2024-12-22 00:56:59 +0800

    Update

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,18 +10,18 @@ include device/lenovo/mt6765-common/BoardConfigCommon.mk
 DEVICE_PATH := device/lenovo/X306FC
 
 # Charger suspend!
-BOARD_CHARGER_ENABLE_SUSPEND := true
+#BOARD_CHARGER_ENABLE_SUSPEND := true
 
 # Display
 TARGET_SCREEN_DENSITY := 160
-TARGET_USES_VULKAN := true
+#TARGET_USES_VULKAN := true
 
 # Kernel
 TARGET_FORECE_PREBUILT_KERNEL := true
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz
 BOARD_PREBUILT_DTBIMAGE_DIR := $(DEVICE_PATH)/prebuilt/dtb
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
-BOARD_VENDOR_KERNEL_MODULES := $(wildcard $(DEVICE_PATH)/prebuilt/lkm/*.ko)
+BOARD_VENDOR_KERNEL_MODULES := $(wildcard $(DEVICE_PATH)/prebuilt/lkm-4.9/*.ko)
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
